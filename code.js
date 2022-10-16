605e1e0fa853d690336fee4437040046498826c1
itsmarii <itsmariiboi@gmail.com>
2022-10-17 00:55:54 +0800

    Created html outline aswell as a code outline for the game

---
+++ code.js
@@ -0,0 +1,25 @@
+const comppoints = 0;
+const userpoints =0;
+
+function getcomputerChoise(){
+    const choices =["Rock","Paper","Scissors"]
+    choise = (Math.random() * choices.length)
+    console.log("test")
+
+    return choise
+}
+
+function playRound(){
+
+}
+
+function game(playRound){
+    for(let i = 0; i<5 ; i++){
+
+    }
+}
+console.log(getcomputerChoise)
+
+const playerSelection =""
+const computerSelection = getcomputerChoise();
+
