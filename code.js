c03cd24a768056069d8d3cb56ecc7db0be568c0d
itsmarii <itsmariiboi@gmail.com>
2022-10-28 07:09:34 +0800

    Created basic functions which will be used to play the rocker paper scissors game

--- code.js
+++ code.js
@@ -1,16 +1,64 @@
-const comppoints = 0;
-const userpoints =0;
+
+const currentpointuser = document.getElementById("scoreuser") ;
+const currentpointcomp = document.getElementById("scorecomp") ;
+console.log(currentpointcomp) ; 
+console.log(currentpointuser) ;
+
+function inputrock(){
+    userinput = "Rock" ;
+    playRound(userinput)
+}
+
+function inputscissor(){
+    userinput = "Scissors" ;
+    playRound(userinput)
+
+}
+
+function inputpaper(){
+    userinput = "Paper" ; 
+    playRound(userinput)
+
+}
+
+
+function scorechange(x){
+   // 0 is tie 1 is comp 2 is user 
+   if (x == "1" )
+    currentpointcomp.innerHTML = '3';
+
+}
 
 function getcomputerChoise(){
-    const choices =["Rock","Paper","Scissors"]
-    choise = (Math.random() * choices.length)
-    console.log("test")
-
+    const choices =["Rock","Paper","Scissors"];
+    random = Math.floor(Math.random()*(3 -1 +1)) + 1
+    choise = choices[random]
+    console.log("choice comptuer made"+choise);
     return choise
 }
 
-function playRound(){
+function playRound(userinput){
+    choice = getcomputerChoise()
+    if (userinput == choise)
+        scorechange(0);
+    if (userinput == "Rock" && choise == "Paper")
+        scorechange(1);
+    else{
+        scorechange(2);
+    }
+    if (userinput == "Paper" && choise == "Scissors")
+        scorechange(1);
+    else{
+        scorechange(2);
+    }
 
+    if (userinput == "Rock" && choise == "Scissors")
+        scorechange(2);
+    else{
+        scorechange(1);
+    }
+    
+    
 }
 
 function game(playRound){
@@ -18,8 +66,3 @@ function game(playRound){
 
     }
 }
-console.log(getcomputerChoise)
-
-const playerSelection =""
-const computerSelection = getcomputerChoise();
-
